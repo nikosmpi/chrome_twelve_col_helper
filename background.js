318219a67ae1a7aa3c9fd45707c344a8a0105e14
Nikos Mpilalis <nikos.mpilalis@mozaik.com>
2022-11-28 15:32:58 +0200

    Add Files

---
+++ background.js
@@ -0,0 +1,80 @@
+function getGridHTML() {
+  //return 
+}
+function toggleGrid() {
+  const gridHTML =`
+<div class="container-fluid test-grid">
+<div class="row">
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+    <div class="col-1">
+    <div></div>
+    </div>
+</div>
+</div>
+<style>
+.test-grid {
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 1000;
+    opacity: 0.5;
+}
+.test-grid  [class*="col-"] div{
+    height: 100vh;
+    background: red;
+}
+</style>
+`;
+
+const htmlGrid = document.getElementsByClassName('test-grid');
+
+  if( htmlGrid.length ) {
+    htmlGrid[0].remove();
+  } else {
+   document.body.innerHTML += gridHTML ;
+  }
+}
+
+
+chrome.action.onClicked.addListener((tab) => {
+
+  if(!tab.url.includes("chrome://")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: toggleGrid
+    });
+  }
+
+});
